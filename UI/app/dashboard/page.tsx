a3f1b00e2568ca372d9aeaa2c6ba5833f4281b7c
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2025-02-12 14:56:46 +0500

    v0.12
    *Dashboard Updated

--- UI/app/dashboard/page.tsx
+++ UI/app/dashboard/page.tsx
@@ -11,8 +11,16 @@ export default function Page(): ReactNode
 {
   return (
     <>
-      <div className=" w-full h-full flex justify-center items-center">
-        <h1 className=" font-secondary font-bold text-5xl"> Hello, Khizer </h1>
+      <div className=" w-full h-full p-6 flex justify-start items-start">
+        <div className=" w-96 h-48 m-2 flex flex-col justify-center items-center rounded-xl bg-quaternary">
+          <h6 className=" m-2 font-secondary font-bold text-3xl"> Assalam Wallikum, </h6>
+          <h6 className=" m-2 font-secondary text-3xl"> Khizer </h6>
+        </div>
+
+        <div className=" w-96 h-48 m-2 flex flex-col justify-center items-center rounded-xl bg-quaternary">
+          <h6 className=" m-2 font-secondary font-bold text-3xl"> Last Entry Time </h6>
+          <p className=" m-2 font-secondary text-lg"> 10/02/2025, 4:00:00 pm </p>
+        </div>
       </div>
     </>
   );
